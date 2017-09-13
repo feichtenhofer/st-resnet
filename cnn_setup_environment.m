952f74e76c615cabe64fb097a52ce782ade9b836
Christoph Feichtenhofer <feichtenhofer@tugraz.at>
2017-09-13 18:16:52 +0200

    add test

--- cnn_setup_environment.m
+++ cnn_setup_environment.m
@@ -3,6 +3,7 @@ function [ opts ] = cnn_setup_environment( varargin )
   run(fullfile(fileparts(mfilename('fullpath')), ...
          'matconvnet','matlab', 'vl_setupnn.m')) ;
 
+  opts.train.gpus = [] ; 
   opts.dataPath = 'data';
   opts.modelPath ='models';
   opts.flowDir = 'data/ucf101/tvl1_flow';
